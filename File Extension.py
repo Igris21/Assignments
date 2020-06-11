a37126608bf0e2cfb84666ec2d0e9e0637d1d80d
Igris21 <55581408+Igris21@users.noreply.github.com>
2020-06-11 18:45:15 +0530

    Update File Extension.py

--- File Extension.py
+++ File Extension.py
@@ -1,18 +1,17 @@
-print("Input .n to exit the loop")
-while True:
-    filename = input("Input the Filename: ")
-    f = filename.split(".")
-    if f[-1]=="py":
-        x="python"
-    elif f[-1]=="C" or f[-1]=="c":
-        x="C"
-    elif f[-1]=="h":
-        x="C++"
-    elif f[-1]=="java":
-        x="Java"
-    elif f[-1]=="n":
-        break
-    else:
-        x="Unknown"
-    print ("The extension of the file is:",x)
+
+filename = input("Input the Filename:")
+f = filename.split(".")
+if f[-1]=="py":
+    x="python"
+elif f[-1]=="C" or f[-1]=="c":
+    x="C"
+elif f[-1]=="h":
+    x="C++"
+elif f[-1]=="java":
+    x="Java"
+elif f[-1]=="n":
+    break
+else:
+    x="Unknown"
+print ("The extension of the file is:",x)
     
