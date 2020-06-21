558458bf8d83bdf5eb0a4cebefff4a725405c8a3
Igris21 <55581408+Igris21@users.noreply.github.com>
2020-06-21 14:35:28 +0530

    Add files via upload

--- repetition.py
+++ repetition.py
@@ -8,5 +8,7 @@ def cf(str1):
         else:
             dict[n] = 1
     sdict = sorted((key,value) for (value, key) in dict.items())
-    print(sdict)
-print(cf(x))
+    for x in sdict:
+        print(x[1], "=", x[0])
+print("")
+cf(x)
